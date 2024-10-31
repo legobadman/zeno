e307e5119a489114684daa373572559a931a7f03
luzh <luzh16371@gmail.com>
2024-10-31 16:38:47 +0800

    temp: work by camera.

--- zenovis/src/bate/GraphicIndicator.cpp
+++ zenovis/src/bate/GraphicIndicator.cpp
@@ -56,30 +56,33 @@ static const char* frag_code = R"(
     }
 )";
 
+using CHAR_VBO_DATA = std::vector<zeno::vec3f>;
+
+struct CHAR_VBO_INFO {
+    zeno::vec3f pos;
+    CHAR_VBO_DATA m_data;
+};
+
 struct PointIndicator final : IGraphicDraw {
-
-    using CHAR_VBO_DATA = std::vector<GLfloat>;
-
     Scene* scene;
     std::vector<zeno::vec3f> m_pos;
     Program* lines_prog;
     std::unique_ptr<Buffer> vbo;
-    std::vector<CHAR_VBO_DATA> m_data;
+    std::vector<CHAR_VBO_INFO> m_data;
 
     explicit PointIndicator(zeno::GeometryObject* pObject, Scene* scene_) : scene(scene_) {
         m_pos = pObject->get_attrs<zeno::vec3f>(zeno::ATTR_POINT, "pos");
         vbo = std::make_unique<Buffer>(GL_ARRAY_BUFFER);
-        init_all_points();
+        init_all_points(pObject);
     }
 
-    void init_all_points() {
+    void init_all_points(zeno::GeometryObject* pObject) {
         size_t n = m_pos.size();
         for (int iPoint = 0; iPoint < n; iPoint++) {
-            //if (iPoint > 0) {
+            //if (iPoint != 12) {
             //    continue;
             //}
             zeno::vec3f basepos = m_pos[iPoint];
-            //float x = basepos[0], y = basepos[1], z = basepos[2];
             std::string drawNum = std::to_string(iPoint);
             GLfloat arr[1024];
             int size = 0;
@@ -104,14 +107,14 @@ struct PointIndicator final : IGraphicDraw {
                 if (i % 2 == 0) {
                     GLfloat xp = arr[i];
                     xp -= xmin;
-                    xp /= width;
-                    xp *= 0.03;
+                    xp /= (width/2);
+                    xp *= 0.008;
                     arr[i] = xp;
                 }
                 else {
                     GLfloat yp = arr[i];
                     yp -= ymin;
-                    yp /= height;
+                    yp /= (height*1.5);
                     yp *= 0.03;
                     arr[i] = yp;
                 }
@@ -123,49 +126,49 @@ struct PointIndicator final : IGraphicDraw {
                 int mem_size = i_end - i_start;
                 assert(mem_size % 2 == 0);
                 int nVertexs = mem_size / 2;
-                CHAR_VBO_DATA mem;
-                mem.reserve(nVertexs * 3);
+                CHAR_VBO_DATA mem(nVertexs);
                 for (int k = 0; k < nVertexs; k++) {
-                    GLfloat xp = basepos[0] + arr[i_start + k * 2];
-                    GLfloat yp = basepos[1] + arr[i_start + k * 2 + 1];
-                    GLfloat zp = basepos[2];
-                    mem.push_back(xp);
-                    mem.push_back(yp);
-                    mem.push_back(zp);
+                    GLfloat xp = arr[i_start + k * 2];
+                    GLfloat yp = arr[i_start + k * 2 + 1];
+                    GLfloat zp = 0;
+                    //先放置在原点，待会再实施旋转和平移
+                    mem[k] = zeno::vec3f(xp, yp, zp);
                 }
-                m_data.push_back(mem);
+                CHAR_VBO_INFO info;
+                info.m_data = mem;
+                info.pos = basepos;
+                m_data.emplace_back(info);
             }
         }
     }
 
     void init_all_points2() {
-        size_t n = m_pos.size();
-        for (int i = 0; i < n; i++) {
-            if (i == 0)
-            {
-                zeno::vec3f basepos = m_pos[i];
-                float x = basepos[0], y = basepos[1], z = basepos[2];
-                GLfloat arr[1024];
-                int size = 0;
-                GLfloat _basepos[3] = { x,y,z };
-                std::string drawNum = std::to_string(i);
-                drawNum = "012345678";
-                int arr_split[64];
-                int split_size = 0;
-                glutGetStrokeString2(GLUT_STROKE_MONO_ROMAN, _basepos, drawNum.c_str(), arr, &size, arr_split, &split_size);
-
-                for (int j = 0; j < split_size; j++) {
-                    int i_start = (j == 0) ? 0 : arr_split[j - 1];
-                    int i_end = arr_split[j];
-                    int mem_size = i_end - i_start;
-                    CHAR_VBO_DATA mem(mem_size);
-                    for (int k = i_start; k < i_end; k++) {
-                        mem[k - i_start] = arr[k];
-                    }
-                    m_data.push_back(mem);
-                }
-            }
-        }
+        //size_t n = m_pos.size();
+        //for (int i = 0; i < n; i++) {
+        //    if (i == 0)
+        //    {
+        //        zeno::vec3f basepos = m_pos[i];
+        //        float x = basepos[0], y = basepos[1], z = basepos[2];
+        //        GLfloat arr[1024];
+        //        int size = 0;
+        //        GLfloat _basepos[3] = { x,y,z };
+        //        std::string drawNum = std::to_string(i);
+        //        drawNum = "012345678";
+        //        int arr_split[64];
+        //        int split_size = 0;
+        //        glutGetStrokeString2(GLUT_STROKE_MONO_ROMAN, _basepos, drawNum.c_str(), arr, &size, arr_split, &split_size);
+        //        for (int j = 0; j < split_size; j++) {
+        //            int i_start = (j == 0) ? 0 : arr_split[j - 1];
+        //            int i_end = arr_split[j];
+        //            int mem_size = i_end - i_start;
+        //            CHAR_VBO_DATA mem(mem_size);
+        //            for (int k = i_start; k < i_end; k++) {
+        //                mem[k - i_start] = arr[k];
+        //            }
+        //            m_data.push_back(mem);
+        //        }
+        //    }
+        //}
     }
 
     void draw() override {
@@ -173,11 +176,42 @@ struct PointIndicator final : IGraphicDraw {
         lines_prog->use();
         scene->camera->set_program_uniforms(lines_prog);
 
-        for (const auto& vbo_data : m_data) {
+        glm::vec3 lodfront = scene->camera->get_lodfront();
+        glm::vec3 lodup = scene->camera->get_lodup();
+        glm::vec3 cam_pos = scene->camera->getPos();
+        glm::vec3 pivot = scene->camera->getPivot();
+
+        zeno::log_info("\n\n\n\n\n");
+        zeno::log_info("lodfront: x={}, y={}, z={}", lodfront[0], lodfront[1], lodfront[2]);
+        zeno::log_info("lodup: x={}, y={}, z={}", lodup[0], lodup[1], lodup[2]);
+        zeno::log_info("pos: x={}, y={}, z={}", cam_pos[0], cam_pos[1], cam_pos[2]);
+        zeno::log_info("pivot: x={}, y={}, z={}", pivot[0], pivot[1], pivot[2]);
+        zeno::log_info("\n\n\n\n\n");
+
+        glm::vec3 uz = glm::normalize(cam_pos);
+        glm::vec3 uy = glm::normalize(lodup);
+        glm::vec3 ux = glm::cross(uy, uz);
+        ux = glm::normalize(ux);
+
+        glm::mat3 rotateM(ux, uy, uz);
+
+        glm::mat4x4 vm = scene->camera->get_view_matrix();
+
+        for (CHAR_VBO_INFO& vbo_data : m_data) {
+            int n = vbo_data.m_data.size();
+            CHAR_VBO_DATA new_vbodata(n);
+            //new_vbodata = vbo_data;
+            for (int i = 0; i < n; i++) {
+                auto pos = vbo_data.m_data[i];
+                glm::vec3 newpos = rotateM * glm::vec3(pos[0], pos[1], pos[2]);
+                newpos += glm::vec3(vbo_data.pos[0], vbo_data.pos[1], vbo_data.pos[2]);
+                newpos += (cam_pos - newpos) * 0.01f;
+                new_vbodata[i] = zeno::vec3f(newpos[0], newpos[1], newpos[2]);
+            }
             vbo->bind();
-            vbo->bind_data(vbo_data.data(), vbo_data.size() * sizeof(GLfloat));
-            vbo->attribute(0, sizeof(float) * 0, sizeof(GLfloat) * 3, GL_FLOAT, 3);
-            CHECK_GL(glDrawArrays(GL_LINE_STRIP, 0, vbo_data.size() / 3));
+            vbo->bind_data(new_vbodata.data(), new_vbodata.size() * sizeof(new_vbodata[0]));
+            vbo->attribute(0, 0, sizeof(GLfloat) * 3, GL_FLOAT, 3);
+            CHECK_GL(glDrawArrays(GL_LINE_STRIP, 0, new_vbodata.size()));
             vbo->unbind();
         }
 
