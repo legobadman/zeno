64a12c15b044098d57f7c196e76ca12b2334f684
luzh <luzh16371@gmail.com>
2024-12-04 16:17:12 +0800

    [fix] resolve ref info from param formulas.

--- zenocore/src/core/INode.cpp
+++ zenocore/src/core/INode.cpp
@@ -1073,15 +1073,20 @@ std::set<std::pair<std::string, std::string>> INode::resolveReferSource(const An
         ctx.spNode = shared_from_this();
         for (auto param_text : refSegments)
         {
+            std::string code = param_text;
+            if (param_text.find('\n') == std::string::npos && param_text.back() != ';') {
+                code = param_text + ';';    //方便通过zfx语句编译
+            }
+
             ZfxContext ctx;
             ctx.spNode = shared_from_this();
             ctx.spObject = nullptr;
-            ctx.code = param_text;
-            ZfxExecute zfx(param_text, &ctx);
+            ctx.code = code;
+            ZfxExecute zfx(code, &ctx);
             int ret = zfx.parse();
             if (ret == 0)
             {
-                ctx.code = param_text;
+                ctx.code = code;
                 std::shared_ptr<ZfxASTNode> astRoot = zfx.getASTResult();
                 std::set<std::pair<std::string, std::string>> paths =
                     funcMgr->getReferSources(astRoot, &ctx);
