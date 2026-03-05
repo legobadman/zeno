5f6c28b683babdf34b940d2c1ff46f82a1464f18
luzh <luzh16371@gmail.com>
2026-03-05 21:21:12 +0800

    [fix] value of object type.

--- zenodes/zs_base/src/Route.cpp
+++ zenodes/zs_base/src/Route.cpp
@@ -11,9 +11,6 @@ struct Route : INode2 {
         if (nd->has_input("input")) {
             nd->set_output_object("output", nd->clone_input_object("input"));
         }
-        if (nd->has_input("SRC")) {
-            nd->set_output_object("DST", nd->clone_input_object("SRC"));
-        }
         return ZErr_OK;
     }
 };
@@ -21,11 +18,9 @@ struct Route : INode2 {
 ZENDEFNODE_ABI(Route,
     Z_INPUTS(
         {"input", _gParamType_IObject},
-        {"SRC", _gParamType_IObject}
     ),
     Z_OUTPUTS(
         {"output", _gParamType_IObject},
-        {"DST", _gParamType_IObject}
     ),
     "layout",
     "",
